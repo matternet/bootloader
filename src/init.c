0519f96d526e760a433eebc2fecb3a1944b872e3
MitchKoch <mitch@matternet.us>
2018-10-19 16:39:59 -0700

    Updates boards for new bootcode source com.matternet.bmu_2.0 com.matternet.bmu_1.0 boards/com.matternet.charger_1.0

--- src/init.c
+++ src/init.c
@@ -18,8 +18,11 @@
 #include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/gpio.h>
 
+#if defined(BOARD_CONFIG_MCU_STM32F3) && defined(BOARD_CONFIG_OSC_HSE_8MHZ)
 static void init_clock_stm32f3_8mhz_hse(void);
+#elif defined(BOARD_CONFIG_MCU_STM32F3) && defined(BOARD_CONFIG_OSC_HSE_24MHZ)
 static void init_clock_stm32f3_24mhz_hse(void);
+#endif
 
 void init_clock(void) {
 #if defined(BOARD_CONFIG_MCU_STM32F3) && defined(BOARD_CONFIG_OSC_HSE_8MHZ)
@@ -31,6 +34,7 @@ void init_clock(void) {
 #endif
 }
 
+#if defined(BOARD_CONFIG_MCU_STM32F3) && defined(BOARD_CONFIG_OSC_HSE_8MHZ)
 static void init_clock_stm32f3_8mhz_hse(void)
 {
     rcc_osc_on(RCC_HSE);
@@ -60,6 +64,7 @@ static void init_clock_stm32f3_8mhz_hse(void)
     rcc_wait_for_sysclk_status(RCC_PLL);
 }
 
+#elif defined(BOARD_CONFIG_MCU_STM32F3) && defined(BOARD_CONFIG_OSC_HSE_24MHZ)
 static void init_clock_stm32f3_24mhz_hse(void)
 {
     rcc_osc_on(RCC_HSE);
@@ -88,3 +93,4 @@ static void init_clock_stm32f3_24mhz_hse(void)
     rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
     rcc_wait_for_sysclk_status(RCC_PLL);
 }
+#endif
